abc7d3e0d5ae76327f76c6cdbd180be29a61107d
gi8 <giottolino@gmail.com>
2023-03-09 16:02:33 +0100

    cleanup

--- hack/perf.sh
+++ hack/perf.sh
@@ -10,6 +10,6 @@ Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel mas
 Aliquam convallis sollicitudin purus. Praesent aliquam, enim at fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus, felis magna fermentum augue, et ultricies lacus lorem varius purus. Curabitur eu amet.
 """
 
-for i in {1..100000}; do
-    echo $var >> test.txt
+for i in {1..100}; do
+    echo $var >> input.txt
 done
