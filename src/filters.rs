713cafe0cb35b0c692a082f96c7b7ea95ecbeed9
gi8 <giottolino@gmail.com>
2023-03-18 10:49:49 +0100

    improve flag_arg

--- src/filters.rs
+++ src/filters.rs
@@ -76,15 +76,20 @@ impl Filters {
         value: Option<&str>,
         iter: &mut std::slice::Iter<String>,
     ) -> Result<(), ParseArgsError> {
-        let flag_arg = match value {
-            Some(value) => value.to_string(), // value is set => --flag=value
-            None => match iter.next() { // value is not set => --flag value
-                Some(next_arg) if !START_PARAMETERS.contains(&next_arg.as_str()) => {
-                    next_arg.to_string()
-                }
-                _ => return Err(ParseArgsError::MissingValue(arg.to_string())),
-            },
-        };
+        let flag_arg: String = value
+            .map_or_else(  // if no value is provided... (was not --prefix=prefix_)
+                || {
+                    // if not, get the next argument as the value
+                    iter.next()
+                        .map(std::string::ToString::to_string) // convert the value to a string
+                        .ok_or_else(|| ParseArgsError::MissingValue(arg.to_string())) // return an error if the value is missing
+                },
+                |s| Ok(s.to_string()), // return the value if it exists
+            )?;
+
+        if START_PARAMETERS.contains(&flag_arg.as_str()) {
+            return Err(ParseArgsError::MissingValue(arg.to_string()));
+        }
 
         match filter {
             Filter::Prefix => {
