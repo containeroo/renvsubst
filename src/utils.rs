8c4fe2818e2e9a4ee63a9d4e9076ec8d6e5ba796
gi8 <giottolino@gmail.com>
2023-03-29 12:31:37 +0200

    move function colorize_text

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,17 @@
-use colored::Colorize;
+use colored::{Color, Colorize};
 
 /// Prints an error message in red.
 #[cfg(not(tarpaulin_include))]
 pub fn print_error(error: &str) {
     eprintln!("{} {}", "ERROR:".red(), error.red());
+}
+
+#[must_use]
+pub fn colorize_text(colored: bool, text: String, color: Color) -> String {
+    if colored {
+        return text.color(color).to_string();
+    }
+    return text;
 }
 
 /// List with all the parameters that can be used to start the program.
